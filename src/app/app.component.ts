ef1f506afc0da8e37193c21ba07c18f181642b8d
Brandon Lakey <ibrandon@Brandons-MacBook-Air.local>
2022-07-24 12:20:02 -0500

    TypeScript_Tips

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Brandon\'s Page';
+  city = "Planet Earth";
+  tag_line = "I AM > I WAS";
+  about_me = "This coding practice is another step towards becoming Hokage!"
+}
